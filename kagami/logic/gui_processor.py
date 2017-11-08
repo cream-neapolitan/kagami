320ded6a89847375fd8d14c2e7b7d75c6db9668d
Neapolitan <yunchan1881@gmail.com>
2017-11-08 22:03:25 +0700

    Implement gui_processor.update_thumb_container
    This code sucks! It needs to be changed later.

--- kagami/logic/gui_processor.py
+++ kagami/logic/gui_processor.py
@@ -1,6 +1,12 @@
+from kagami.logic import reflector
+from PIL.ImageTk import PhotoImage
 '''Code used for modifying UI'''
 
 
 def update_thumb_container(master):
-    master.infotext.proc_image_container.label.configure(
-        text=master.reflection_mode.get())
+    thumb = reflector.reflect_image(
+        master.infotext.thumb_original, master.reflection_mode.get())
+    widget = PhotoImage(thumb)
+
+    master.infotext.refl_image_container.label.image = widget
+    master.infotext.refl_image_container.label.configure(image=widget)
